9ce789ffee0b508e276af8d1e97bb0ce4f7590a4
Markus Ankenbrand <markus@ankenbrand.me>
2022-03-03 19:26:33 +0100

    Add programming task

---
+++ two_lists.py
@@ -0,0 +1,7 @@
+# Task: given two lists, and one element of the first list, print the matching element in the second
+
+# The two lists:
+genes = ["bla", "blub", "foo", "bar", "baz"]
+species = ["mouse", "horse", "bee", "owl", "rabbit"]
+
+# Find the species for gene 'foo'
