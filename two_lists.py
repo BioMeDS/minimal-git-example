b0c155658ba13023a16ac203fe34211074379369
Markus Ankenbrand <markus@ankenbrand.me>
2022-03-03 19:31:26 +0100

    Solve programming task using enumerate

--- two_lists.py
+++ two_lists.py
@@ -5,6 +5,6 @@ genes = ["bla", "blub", "foo", "bar", "baz"]
 species = ["mouse", "horse", "bee", "owl", "rabbit"]
 
 # Find the species for gene 'foo'
-for i in range(len(genes)):
-    if genes[i] == 'foo':
-        print(species[i])
+for i, gene in enumerate(genes):
+    if gene == 'foo':
+        printe(species[i])
